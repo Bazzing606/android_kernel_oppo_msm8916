2907cf9acb77059233a1c0e012cc83b992f30292
Belive <erzatp720@gmail.com>
2020-06-15 17:52:05 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
-# By Yasir-siddiqui
-# Yasir-siddiqui
-# By Yasir-siddiqui
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Bazzing606/android_kernel_oppo_msm8916 -b lineage-17.1 kernel
 cd kernel
-git clone --depth=1 https://github.com/crDroidMod/android_prebuilts_clang_host_linux-x86_clang-6032204 clang
-git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
-git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
-git clone --depth=1 https://github.com/Bazzing606/AnyKernel3 -b Ten AnyKernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/Yasir-siddiqui/AnyKernel3 AnyKernel
 echo "Done"
-KERNEL_DIR=$(pwd)
-IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
-PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
+PATH="${PWD}/clang/bin:$PATH"
+export LD="clang/bin/ld.lld"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=ErzaTriana
-export KBUILD_BUILD_USER="ErzaCircleCI"
+export KBUILD_BUILD_HOST=notkernel
+export KBUILD_BUILD_USER="Yasir"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
@@ -31,14 +26,17 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• SadKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0AFor device <b>Asus Zenfone Max Pro M2</b> (X01BD/A)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+        -d text="<b>• NotKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0AFor device <b>Xiaomi Redmi note7/7s</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
 }
 # Push kernel to channel
 function push() {
-    cd AnyKernel || exit 1
+    cd AnyKernel
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id"
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
 # Fin Error
 function finerr() {
@@ -51,19 +49,19 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-   make -j$(nproc) O=out ARCH=arm64 Lineage_a37f_defconfig
-   make -j$(nproc) O=out \
-                    ARCH=arm64 \
-                    CC=clang \
-                    CLANG_TRIPLE=aarch64-linux-gnu- \
-                    CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+   make O=out ARCH=arm64 lineage_a37f_defconfig
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+			     CC=clang \
+			     LD=ld.lld \
+			     CROSS_COMPILE=aarch64-linux-gnu- \
+			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Alive_Q-X01BD-${TANGGAL}.zip *
+    zip -r9 NotKernel-lavender-${TANGGAL}.zip *
     cd .. 
 }
 sticker
@@ -73,5 +71,3 @@ zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
 push
-
-echo "All Succsess"
