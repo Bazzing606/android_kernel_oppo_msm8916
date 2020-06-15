5bcc85016f82a811eec8bfe1fc5d10e85ded1358
Belive <erzatp720@gmail.com>
2020-06-15 17:28:51 +0700

    Create build.sh

---
+++ .circleci/build.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+# By Yasir-siddiqui
+# Yasir-siddiqui
+# By Yasir-siddiqui
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/Bazzing606/android_kernel_oppo_msm8916 -b lineage17.1 kernel
+cd kernel
+git clone --depth=1 https://github.com/crDroidMod/android_prebuilts_clang_host_linux-x86_clang-6032204 clang
+git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
+git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
+git clone --depth=1 https://github.com/Bazzing606/AnyKernel3 -b Ten AnyKernel
+echo "Done"
+KERNEL_DIR=$(pwd)
+IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=ErzaTriana
+export KBUILD_BUILD_USER="Erza@CircleCI"
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
+        -d chat_id=$chat_id
+}
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• SadKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0AFor device <b>Asus Zenfone Max Pro M2</b> (X01BD/A)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+   make -j$(nproc) O=out ARCH=arm64 Lineage_a37f_defconfig
+   make -j$(nproc) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-android- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Alive_Q-X01BD-${TANGGAL}.zip *
+    cd .. 
+}
+sticker
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
+
+echo "All Succsess"
